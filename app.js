1e30d329c3d6a5900b19d91bec41174d199b33a1
Neagu Tudor <tudorneagu7@gmail.com>
2022-09-16 22:14:19 +0300

    added the validation for the form and logging the info to the console

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const username = document.getElementById("username");
 const password = document.getElementById("password");
 const email = document.getElementById("email");
 const password2 = document.getElementById("password2");
+const message = document.getElementById("message");
+const free = document.getElementById("subscription-free");
+const paid = document.getElementById("subscription-paid");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -15,6 +18,28 @@ function checkInputs() {
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
   const password2Value = password2.value.trim();
+  const messageValue = message.value.trim();
+
+  if (
+    username.checkValidity() &&
+    email.checkValidity() &&
+    password.checkValidity() &&
+    message.checkValidity() &&
+    password2.checkValidity()
+  ) {
+    console.log(`Username: ${usernameValue}`);
+    console.log(`Email: ${emailValue}`);
+    console.log(`Message: ${messageValue}`);
+    console.log(`Password: ${passwordValue}`);
+    console.log(`Passoword2: ${password2Value}`);
+  }
+
+  if (free.checked) {
+    console.log("Free subscription");
+  } else if (paid.checked) {
+    console.log("Paid Subscription");
+  }
+
 
   if (usernameValue === "") {
     setErrorFor(username, "Username cannot be blank");
@@ -37,12 +62,37 @@ function checkInputs() {
   }
 
   if (password2Value === "") {
-    setErrorFor(password2, "Password2 cannot be blank");
+    setErrorFor(password2, "Password cannot be blank");
   } else if (passwordValue !== password2Value) {
     setErrorFor(password2, "Passwords does not match");
   } else {
-    setSuccessFor(password2);
+    setSuccesFor(password2);
   }
+
+  if (messageValue === "") {
+    setErrorFor(message, "Message cannot be blank");
+  } else {
+    setSuccesFor(message);
+  }
+  if (!free.checked && !paid.checked) {
+    setErrorFor(free, "Please select a subscription");
+    setErrorFor(paid, "Please select a subscription");
+  } else if ((free.checked && !paid, checked)) {
+    setSuccesFor(free);
+  } else {
+    setSuccesFor(paid);
+  }
+
+  if (!free.checked && paid.checked) {
+    setErrorFor(free, "Please choose a subscription");
+  } else if ((!paid.checked && free, checked)) {
+    setErrorFor(paid, "Please choose a subscription");
+  }
+
+if(){
+    displayMessage();
+}
+
 }
 
 function setErrorFor(input, message) {
@@ -61,4 +111,8 @@ function isEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
   );
+}
+
+function displayMessage(){
+    
 }
